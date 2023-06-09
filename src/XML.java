14929ad9de229b9c0aa5c35eab4447eddb499b32
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2023-06-09 16:07:04 +0200

    vite fait

--- src/XML.java
+++ src/XML.java
@@ -6,7 +6,6 @@ public final class XML {
      * @return
      */
     public static String[] parseXMLLine(String line){
-
         String ligneIntermediaire = line.split("<")[1]; // Dans <port>80</port>, prend l'élément d'indice 1 du tableau de String donc après le < pour avoir : port>80
         String[] res = ligneIntermediaire.split(">"); // On resplit pour prendre le port et le 80 seulement on a donc un tableau ["port", "80"]
         return res;
