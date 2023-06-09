782374d8a2c744fd2247aa07cc7a722144a9e0d4
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2023-06-09 16:44:30 +0200

    petit modif serveurhttp

--- src/HTTPServer.java
+++ src/HTTPServer.java
@@ -8,7 +8,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import javax.imageio.ImageIO;
 
 public class HTTPServer {
